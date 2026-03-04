a0910ec7a06fc9454945cbf0ba469b8dd2e1ce2f
Pedro Monteiro <83165668+pedroafmonteiro@users.noreply.github.com>
2026-03-04 17:35:50 +0000

    added missing checkbox for the unit cube in the gui

--- tp2/MyInterface.js
+++ tp2/MyInterface.js
@@ -1,30 +1,30 @@
-import {CGFinterface, dat} from '../lib/CGF.js';
+import { CGFinterface, dat } from "../lib/CGF.js";
 
 /**
-* MyInterface
-* @constructor
-*/
+ * MyInterface
+ * @constructor
+ */
 export class MyInterface extends CGFinterface {
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
-    init(application) {
-        // call CGFinterface init
-        super.init(application);
-        
-        // init GUI. For more information on the methods, check:
-        // https://github.com/dataarts/dat.gui/blob/master/API.md
-        this.gui = new dat.GUI();
+  init(application) {
+    // call CGFinterface init
+    super.init(application);
 
-        //Checkbox element in GUI
-        this.gui.add(this.scene, 'displayAxis').name('Display Axis');
+    // init GUI. For more information on the methods, check:
+    // https://github.com/dataarts/dat.gui/blob/master/API.md
+    this.gui = new dat.GUI();
 
-        this.gui.add(this.scene, 'displayTangram').name('Display Tangram');
-        
-        //Slider element in GUI
-        this.gui.add(this.scene, 'scaleFactor', 0.1, 5).name('Scale Factor');
+    //Checkbox element in GUI
+    this.gui.add(this.scene, "displayAxis").name("Display Axis");
+    this.gui.add(this.scene, "displayUnitCube").name("Display Unit Cube");
+    this.gui.add(this.scene, "displayTangram").name("Display Tangram");
 
-        return true;
-    }
+    //Slider element in GUI
+    this.gui.add(this.scene, "scaleFactor", 0.1, 5).name("Scale Factor");
+
+    return true;
+  }
 }
