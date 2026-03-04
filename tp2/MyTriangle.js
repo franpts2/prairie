f41e0b0b8ba6f11abd1fbb65ad949712cf316901
Francisca Vilares Portugal <up202303640@edu.fe.up.pt>
2026-03-04 18:02:33 +0000

    Quick fix to MyTriangle

--- tp2/MyTriangle.js
+++ tp2/MyTriangle.js
@@ -20,6 +20,7 @@ export class MyTriangle extends CGFobject {
 		//Counter-clockwise reference of vertices
 		this.indices = [
 			0, 1, 2,
+            2, 1, 0,
 		];
 
 		//The defined indices (and corresponding vertices)
