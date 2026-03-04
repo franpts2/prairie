319fe98e0afee9d412eb7776b8126c2ed91f8fd5
Francisca Vilares Portugal <up202303640@edu.fe.up.pt>
2026-03-04 17:05:26 +0000

    Implemented UnitCube

--- tp2/MyScene.js
+++ tp2/MyScene.js
@@ -1,6 +1,6 @@
 import { CGFscene, CGFcamera, CGFaxis } from "../lib/CGF.js";
 import { MyTangram } from "./MyTangram.js";
-import { MyQuad } from "./MyQuad.js";
+import { MyUnitCube } from "./MyUnitCube.js";
 
 /**
  * MyScene
@@ -27,12 +27,12 @@ export class MyScene extends CGFscene {
     //Initialize scene objects
     this.axis = new CGFaxis(this);
     this.tangram = new MyTangram(this);
-    this.quad = new MyQuad(this);
+    this.unitCube = new MyUnitCube(this);
 
     //Objects connected to MyInterface
     this.displayAxis = true;
-    this.displayTangram = false;
-    this.displayQuad = true;
+    this.displayTangram = true;
+    this.displayUnitCube = true;
     this.scaleFactor = 1;
   }
   initLights() {
@@ -99,8 +99,8 @@ export class MyScene extends CGFscene {
 
     // ---- BEGIN Primitive drawing section
     
-    if (this.displayTangram) this.tangram.display();
-    if (this.displayQuad) this.quad.display();
+    //if (this.displayTangram) this.tangram.display();
+    if (this.displayUnitCube) this.unitCube.display();
 
     // ---- END Primitive drawing section
   }
