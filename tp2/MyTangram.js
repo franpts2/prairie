13abdc11dbfb7984796be3b19bc5431d9fc350c3
Carolina Alves Ferreira <up202303547@edu.fe.up.pt>
2026-03-05 20:18:34 +0000

    Added MyTangram screenshot and did a minor adjustment the parallelogram

--- tp2/MyTangram.js
+++ tp2/MyTangram.js
@@ -78,6 +78,7 @@ export class MyTangram extends CGFobject {
 
     this.scene.pushMatrix();
     this.scene.translate(0.0, 2.8, 0.0);
+    this.scene.rotate(Math.PI, 0.0, 1.0, 0.0);
     this.scene.rotate((Math.PI/4 + 0.46365), 0.0, 0.0, 1.0);
     this.parallelogram.display();
     this.scene.popMatrix();
