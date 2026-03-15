30fa6fedf6d254a6abc7ae88e410021d1e06e160
Francisca Vilares Portugal <up202303640@edu.fe.up.pt>
2026-03-15 18:02:12 +0000

    Quick fix

--- tp3/MyParallelogram.js
+++ tp3/MyParallelogram.js
@@ -27,10 +27,10 @@ export class MyParallelogram extends CGFobject {
 		];
 
 		this.normals = [
-			0, 0, 1,
-			0, 0, 1,
-			0, 0, 1,
-			0, 0, 1,
+			0, 0, -1,
+			0, 0, -1,
+			0, 0, -1,
+			0, 0, -1,
 		];
 
 		//The defined indices (and corresponding vertices)
