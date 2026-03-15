8865cc3dc58c5b8b2ef48fd1070ba28518d7a9af
Francisca Vilares Portugal <up202303640@edu.fe.up.pt>
2026-03-15 18:31:08 +0000

    Added green material to Tangram

--- tp3/MyTangram.js
+++ tp3/MyTangram.js
@@ -1,4 +1,4 @@
-import { CGFobject } from "../lib/CGF.js";
+import { CGFobject, CGFappearance } from "../lib/CGF.js";
 import { MyDiamond } from "./MyDiamond.js";
 import { MyTriangle } from "./MyTriangle.js";
 import { MyParallelogram } from "./MyParallelogram.js";
@@ -21,7 +21,18 @@ export class MyTangram extends CGFobject {
     this.blueTriangle = new MyTriangleBig(scene);
     this.redTriangle = new MyTriangleSmall(scene);
     this.purpleTriangle = new MyTriangleSmall(scene);
+
+    this.initMaterials();
   }
+
+  initMaterials() {
+          // Green
+          this.greenMaterial = new CGFappearance(this.scene);
+          this.greenMaterial.setAmbient(0, 1.0, 0, 1.0);
+          this.greenMaterial.setDiffuse(0, 1.0, 0, 1.0);
+          this.greenMaterial.setSpecular(1, 1, 1, 1.0);
+          this.greenMaterial.setShininess(10.0);
+      }
 
   display() {
 
@@ -72,7 +83,8 @@ export class MyTangram extends CGFobject {
     // ---- BEGIN Primitive drawing section
     this.scene.pushMatrix();
     this.scene.multMatrix(trans);
-    this.scene.multMatrix(rot);    
+    this.scene.multMatrix(rot);   
+    this.greenMaterial.apply(); 
     this.diamond.display();
     this.scene.popMatrix();
 
@@ -80,35 +92,41 @@ export class MyTangram extends CGFobject {
     this.scene.translate(0.0, 2.8, 0.0);
     this.scene.rotate(Math.PI, 0.0, 1.0, 0.0);
     this.scene.rotate((Math.PI/4 + 0.46365), 0.0, 0.0, 1.0);
+    this.yellowMaterial.apply();
     this.parallelogram.display();
     this.scene.popMatrix();
 
     this.scene.pushMatrix();
     this.scene.translate(0.0, 1.4, 0.0);
     this.scene.rotate(Math.PI/4, 0.0, 0.0, 1.0);
+    this.pinkMaterial.apply();
     this.pinkTriangle.display();
     this.scene.popMatrix();
 
     this.scene.pushMatrix();
     this.scene.translate(-1.0,-1.0,0.0);
     this.scene.rotate(-Math.PI/2,0.0,0.0,1.0);
+    this.orangeMaterial.apply();
     this.orangeTriangle.display();
     this.scene.popMatrix();
 
     this.scene.pushMatrix();
     this.scene.translate(0.0,-4,0.0);
+    this.blueMaterial.apply();
     this.blueTriangle.display();
     this.scene.popMatrix();
 
     this.scene.pushMatrix();
     this.scene.translate(1.0,0.0,0.0);
     this.scene.rotate(Math.PI/2,0.0,0.0,1.0);
+    this.redMaterial.apply();
     this.redTriangle.display();
     this.scene.popMatrix();
 
     this.scene.pushMatrix();
-    this.scene.translate(1.0,0.-2,0.0);
+    this.scene.translate(1.0,-2,0.0);
     this.scene.rotate(Math.PI/2,0.0,0.0,1.0);
+    this.purpleMaterial.apply();
     this.purpleTriangle.display();
     this.scene.popMatrix();
     // ---- END Primitive drawing section
