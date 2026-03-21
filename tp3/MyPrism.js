9e097d6f7b51d1afabad9123ec75171df797af3f
Francisca Vilares Portugal <up202303640@edu.fe.up.pt>
2026-03-21 19:16:40 +0000

    Added variable number of stacks to prism

--- tp3/MyPrism.js
+++ tp3/MyPrism.js
@@ -6,9 +6,10 @@ import { CGFobject } from '../lib/CGF.js';
  * @param slices - number of divisions around the Y axis
  */
 export class MyPrism extends CGFobject {
-    constructor(scene, slices) {
+    constructor(scene, slices, stacks) {
         super(scene);
         this.slices = slices;
+        this.stacks = stacks;
         this.initBuffers();
     }
     initBuffers() {
@@ -16,39 +17,47 @@ export class MyPrism extends CGFobject {
         this.indices = [];
         this.normals = [];
 
-        var ang = 0;
         var alphaAng = 2 * Math.PI / this.slices;
+        var stackStep = 1.0 / this.stacks;
 
-        for (var i = 0; i < this.slices; i++) {
-            var sa = Math.sin(ang);
-            var saa = Math.sin(ang + alphaAng);
-            var ca = Math.cos(ang);
-            var caa = Math.cos(ang + alphaAng);
+        for (var j = 0; j < this.stacks; j++) {
+            var ang = 0;
+            var z1 = j*stackStep;       //start of curr stack
+            var z2 = (j+1)*stackStep;   //end of curr stack
 
-            
-            this.vertices.push(ca, -sa, 0); //0
-            this.vertices.push(caa, -saa, 0); //1
-            this.vertices.push(ca, -sa, 1);  //2
-            this.vertices.push(caa, -saa, 1); //3
-            
 
-            // this.indices.push(4*i, (4*i+1), (4*i+2)); // 0, 1, 2
-            this.indices.push((4*i+2), (4*i+1), 4*i); // 2, 1, 0
-            this.indices.push((4*i+2), (4*i+3), (4*i+1)); // 2, 3, 1
-            // this.indices.push((4*i+3), (4*i+2), (4*i+1)); // 1, 3, 2
+            for (var i = 0; i < this.slices; i++) {
+                var sa = Math.sin(ang);
+                var saa = Math.sin(ang + alphaAng);
+                var ca = Math.cos(ang);
+                var caa = Math.cos(ang + alphaAng);
 
-            
-            // normals
-            var midAng = ang + alphaAng / 2; //perpendical to the face------
-            var nx = Math.cos(midAng);
-            var ny = -Math.sin(midAng);
+                // VERTICES
+                this.vertices.push(ca, -sa, z1);    //0
+                this.vertices.push(caa, -saa, z1);  //1
+                this.vertices.push(ca, -sa, z2);    //2
+                this.vertices.push(caa, -saa, z2);  //3
+                
+                // INDICES
+                var baseIndex = 4 * (j * this.slices + i)
 
-            this.normals.push(nx, ny, 0);
-            this.normals.push(nx, ny, 0);
-            this.normals.push(nx, ny, 0);
-            this.normals.push(nx, ny, 0);
+                // this.indices.push(baseIndex, (baseIndex+1), (baseIndex+2));      // 0, 1, 2
+                this.indices.push((baseIndex+2), (baseIndex+1), baseIndex);         // 2, 1, 0
+                this.indices.push((baseIndex+2), (baseIndex+3), (baseIndex+1));     // 2, 3, 1
+                // this.indices.push((baseIndex+3), (baseIndex+2), (baseIndex+1));  // 1, 3, 2
 
-            ang += alphaAng;
+                // NORMALS
+                var midAng = ang + alphaAng / 2; //perpendical to the face------
+                var nx = Math.cos(midAng);
+                var ny = -Math.sin(midAng);
+
+                this.normals.push(nx, ny, 0);
+                this.normals.push(nx, ny, 0);
+                this.normals.push(nx, ny, 0);
+                this.normals.push(nx, ny, 0);
+
+                ang += alphaAng;
+            }   
         }
 
 
