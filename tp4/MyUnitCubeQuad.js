e5351c1d080cb26216620581c58699819f233e16
Francisca Vilares Portugal <up202303640@edu.fe.up.pt>
2026-04-06 22:45:37 +0100

    Fixed rotations of unit cube

--- tp4/MyUnitCubeQuad.js
+++ tp4/MyUnitCubeQuad.js
@@ -56,7 +56,7 @@ export class MyUnitCubeQuad extends CGFobject {
       this.left.bind();
     }
     this.scene.translate(-0.5, 0, 0);
-    this.scene.rotate(Math.PI / 2, 0.0, 1.0, 0.0);
+    this.scene.rotate(-Math.PI / 2, 0.0, 1.0, 0.0);
     this.quad.display();
     this.scene.popMatrix();
 
@@ -66,7 +66,7 @@ export class MyUnitCubeQuad extends CGFobject {
       this.right.bind();
     }
     this.scene.translate(0.5, 0, 0);
-    this.scene.rotate(-Math.PI / 2, 0.0, 1.0, 0.0);
+    this.scene.rotate(Math.PI / 2, 0.0, 1.0, 0.0);
     this.quad.display();
     this.scene.popMatrix();
 
