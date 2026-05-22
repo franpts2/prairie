554fbc6a5b859a007e6d630e4ac5e12007455c49
Francisca Vilares Portugal <up202303640@edu.fe.up.pt>
2026-05-22 17:29:07 +0100

    Quick fix

--- project/elements/wagon/MyAxle.js
+++ project/elements/wagon/MyAxle.js
@@ -1,7 +1,7 @@
 import { CGFobject, CGFappearance } from '../../../lib/CGF.js';
 import { MyCylinder } from '../../shapes/MyCylinder.js';
 
-export class Axle extends CGFobject {
+export class MyAxle extends CGFobject {
     constructor(scene) {
         super(scene);
         this.axle = new MyCylinder(scene, 12, 1, true);
