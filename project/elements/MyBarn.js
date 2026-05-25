4c5e94f93bea407414e48dc043c2142af202b813
Pedro Monteiro <up202307242@edu.fe.up.pt>
2026-05-25 22:56:39 +0100

    add MyPrism class for barn roof structure

--- project/elements/MyBarn.js
+++ project/elements/MyBarn.js
@@ -1,5 +1,6 @@
 import { CGFobject, CGFappearance } from '../../lib/CGF.js';
 import { MyUnitCube } from '../shapes/MyUnitCube.js';
+import { MyPrism } from '../shapes/MyPrism.js';
 
 export class MyBarn extends CGFobject {
     constructor(scene, x = 20, z = -20) {
@@ -11,8 +12,11 @@ export class MyBarn extends CGFobject {
         this.width = 10;
         this.height = 8;
         this.length = 12;
+        this.roofHeight = 4;
 
         this.walls = new MyUnitCube(scene, 4, 3, 5);
+
+        this.roof = new MyPrism(scene, 4, 2, 5);
 
         this.woodAppearance = new CGFappearance(scene);
         this.woodAppearance.setAmbient(0.4, 0.3, 0.2, 1.0);
@@ -21,6 +25,14 @@ export class MyBarn extends CGFobject {
         this.woodAppearance.setShininess(5.0);
         this.woodAppearance.setTexture(scene.assetManager.getTexture('wood'));
         this.woodAppearance.setTextureWrap('REPEAT', 'REPEAT');
+
+        this.darkWoodAppearance = new CGFappearance(scene);
+        this.darkWoodAppearance.setAmbient(0.15, 0.08, 0.04, 1.0);
+        this.darkWoodAppearance.setDiffuse(0.25, 0.15, 0.08, 1.0);
+        this.darkWoodAppearance.setSpecular(0.05, 0.05, 0.05, 1.0);
+        this.darkWoodAppearance.setShininess(2.0);
+        this.darkWoodAppearance.setTexture(scene.assetManager.getTexture('wood'));
+        this.darkWoodAppearance.setTextureWrap('REPEAT', 'REPEAT');
     }
 
     display() {
@@ -31,9 +43,15 @@ export class MyBarn extends CGFobject {
         this.scene.pushMatrix();
         this.scene.translate(this.x, this.y + this.height / 2, this.z);
         this.scene.scale(this.width, this.height, this.length);
-
         this.woodAppearance.apply();
         this.walls.display();
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+        this.scene.translate(this.x, this.y + this.height, this.z);
+        this.scene.scale(this.width + 0.6, this.roofHeight, this.length + 0.4);
+        this.darkWoodAppearance.apply();
+        this.roof.display();
         this.scene.popMatrix();
     }
 }
