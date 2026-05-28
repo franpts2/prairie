edea03fbdba96c1f1f95c661576401b41bd8f095
Francisca Vilares Portugal <up202303640@edu.fe.up.pt>
2026-05-28 11:00:10 +0100

    Refactored textures folder

--- project/utils/AssetManager.js
+++ project/utils/AssetManager.js
@@ -25,8 +25,8 @@ export class AssetManager {
             rock4: "./textures/rocks/rock4.png",
             wood: "./textures/wood.png",
             leaves: "./textures/leaves.png",
-            barnDoor: "./textures/barndoor.png",
-            barnWindow: "./textures/barnwindow.png",
+            barnDoor: "./textures/barn/barndoor.png",
+            barnWindow: "./textures/barn/barnwindow.png",
             hay: "./textures/hay.png"
         };
     }
