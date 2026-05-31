1e7ab9583e3bd1f45f7e91c2c54e185cf836ff29
Pedro Monteiro <up202307242@edu.fe.up.pt>
2026-05-31 18:04:36 +0100

    changed light position

--- project/elements/sky/MySun.js
+++ project/elements/sky/MySun.js
@@ -5,7 +5,7 @@ export class MySun {
   }
 
   initLight() {
-    this.light.setPosition(0.45, 0.89, 0.16, 0);
+    this.light.setPosition(-0.3217, 0.9468, -0.0014, 0);
     this.light.setAmbient(0.3, 0.3, 0.3, 1.0);
     this.light.setDiffuse(1.0, 1.0, 1.0, 1.0);
     this.light.setSpecular(1.0, 1.0, 1.0, 1.0);
